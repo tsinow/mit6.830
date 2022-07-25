14ee3b9827c06649ebe298fffdaa8b5d403601bb
tsinow <qingwuliu233@gmail.com>
2022-07-25 11:22:39 +0800

    HeapPage

--- src/java/simpledb/storage/RecordId.java
+++ src/java/simpledb/storage/RecordId.java
@@ -19,8 +19,13 @@ public class RecordId implements Serializable {
      * @param tupleno
      *            the tuple number within the page.
      */
+    private PageId pageId;
+    private int tupleNo;
+
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
+        pageId=pid;
+        tupleNo=tupleno;
     }
 
     /**
@@ -28,7 +33,7 @@ public class RecordId implements Serializable {
      */
     public int getTupleNumber() {
         // some code goes here
-        return 0;
+        return tupleNo;
     }
 
     /**
@@ -36,7 +41,7 @@ public class RecordId implements Serializable {
      */
     public PageId getPageId() {
         // some code goes here
-        return null;
+        return pageId;
     }
 
     /**
@@ -48,7 +53,11 @@ public class RecordId implements Serializable {
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        if (o instanceof RecordId){
+            RecordId recordId= (RecordId) o;
+            return recordId.hashCode()==hashCode();
+        }
+        return false;
     }
 
     /**
@@ -60,8 +69,7 @@ public class RecordId implements Serializable {
     @Override
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        return (pageId+"_"+tupleNo).hashCode();
     }
 
 }
